2e3bdc723d42ccc8f816dd9b9db205d8d5b4d751
nikita hunko <nikita.hunko@nure.ua>
2020-10-07 23:14:51 +0300

    Connected to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://nike:123e123@cluster0.toc2y.mongodb.net/<dbname>?retryWrites=true&w=majority',
+()=> console.log('connected to db!'));
 
 const authRoute = require('./routes/auth');
 
