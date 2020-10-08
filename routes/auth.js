3e6c43a23fbfcaeb2e3524060ad9b489da4d6ee8
nikita hunko <nikita.hunko@nure.ua>
2020-10-08 23:55:46 +0300

    Added user-model

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
-
+const User = require('../model/user')
 router.post('/register', (req,res)=>{
-    res.send('Register');
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
 })
 
 // router.post('/login', (req,res)=>{
